01d23c79ab0c7dd4fc7f8930115d9d008e06a3e9
andrasSagi <andras.sagi8128@gmail.com>
2018-07-18 10:51:32 +0200

    Add example data

--- src/main/java/com/codecool/shitwishorder/service/InitializerBean.java
+++ src/main/java/com/codecool/shitwishorder/service/InitializerBean.java
@@ -3,10 +3,15 @@ package com.codecool.shitwishorder.service;
 import com.codecool.shitwishorder.model.ShitWishOrder;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class InitializerBean {
 
     public InitializerBean(ShitWishOrderService shitWishOrder) {
-        shitWishOrder.saveOrder(new ShitWishOrder());
+        Map orderMap = new HashMap<Integer, Integer>();
+        orderMap.put(3, 3);
+        shitWishOrder.saveOrder(new ShitWishOrder(1, orderMap));
     }
 }
