2fe2939fad6d3bde9c33c4216378cbfbacaf79bb
Peter <kovacs.g.peter@gmail.com>
2018-07-19 15:25:56 +0200

    controller's return type migrated to responseentity

--- src/main/java/com/codecool/shitwishorder/controller/OrderController.java
+++ src/main/java/com/codecool/shitwishorder/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.codecool.shitwishorder.model.ShitWishOrder;
 import com.codecool.shitwishorder.service.ShitWishOrderService;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
 
@@ -17,21 +19,25 @@ public class OrderController {
     ShitWishOrderService shitWishOrderService;
 
     @GetMapping(value = "/orders/{id}")
-    public String getOrder(@PathVariable("id") Long id, @RequestHeader Map<String, String> header) {
+    public ResponseEntity getOrder(@PathVariable("id") Long id, @RequestHeader Map<String, String> header) {
+
         if (shitWishOrderService.findById(id, header.get("Authorization")) != null) {
-            ShitWishOrder order = shitWishOrderService.findById(id, header.get("Authorization"));
-            String ret = shitWishOrderService.jsonStringBuilder(order);
-            return ret;
-        } else {
+                ShitWishOrder order = shitWishOrderService.findById(id, header.get("Authorization"));
+//                String ret = shitWishOrderService.jsonStringBuilder(order);
+                ResponseEntity<ShitWishOrder> response = new ResponseEntity(order, HttpStatus.OK);
+
+                return response; }
+        else {
             JSONObject error = new JSONObject();
             error.put("error", "no such order, or invalid token");
-            return error.toString();
+            ResponseEntity<String> response = new ResponseEntity(error.toString(), HttpStatus.BAD_REQUEST);
+            return response;
         }
     }
 
 
     @PostMapping(value = "/orders")
-    public String postOrder(@RequestParam("zipcode") String zipcode, @RequestParam("country") String country,
+    public ResponseEntity postOrder(@RequestParam("zipcode") String zipcode, @RequestParam("country") String country,
                             @RequestParam("city") String city, @RequestParam("street") String street,
                             @RequestParam("products") String productString,  @RequestHeader Map<String, String> header) {
         Map<Integer, Integer> products = new HashMap<Integer, Integer>();
@@ -42,11 +48,13 @@ public class OrderController {
         }
         ShitWishOrder order = shitWishOrderService.saveOrder(new ShitWishOrder(zipcode, country, city, street, products), header.get(("Authorization")));
         if (order!=null){
-            return shitWishOrderService.jsonStringBuilder(order);
-        } else {
+            ResponseEntity<ShitWishOrder> response = new ResponseEntity(order, HttpStatus.OK);
+            return response; }
+         else {
             JSONObject error = new JSONObject();
-            error.put("error", "Token Expired or Invalid Token");
-            return error.toString();
+            error.put("error", "no such order, or invalid token");
+            ResponseEntity<JSONObject> response = new ResponseEntity(error.toString(), HttpStatus.BAD_REQUEST);
+            return response;
             }
         }
 
